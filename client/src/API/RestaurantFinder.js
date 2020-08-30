3a218b72b6458aa0ae504ad58b3ac97138695c03
cristianying <37324199+cristianying@users.noreply.github.com>
2020-08-30 22:33:03 +0800

    deploymemt

--- client/src/API/RestaurantFinder.js
+++ client/src/API/RestaurantFinder.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
 
-    baseURL: process.env.DATABASE_URL || "http://localhost:4001/api/v1/restaurants",
+    baseURL: process.env.DATABASE_URL.anchor("/api/v1/restaurants") || "http://localhost:4001/api/v1/restaurants",
 }) 
+
